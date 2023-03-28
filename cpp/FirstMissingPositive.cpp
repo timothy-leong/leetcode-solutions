9477312c07fba01a2dda07ee812da1ca9516f33f
Timothy Leong <leongjiahaotimothy@hotmail.com>
2023-03-28 10:33:02 +0800

    Add first missing positive

---
+++ cpp/FirstMissingPositive.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        for (size_t i{}; i < nums.size();)
+        {
+            // If i + 1 == nums[i], there's no need to swap.
+            // If nums[nums[i] - 1] == nums[i] already, there's no need to swap also.
+            if (i + 1 != nums[i] && 1 <= nums[i] && nums[i] <= nums.size() && nums[nums[i] - 1] != nums[i])
+            {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+            else
+            {
+                ++i;
+            }
+        }
+
+        for (size_t i{}; i < nums.size(); ++i)
+        {
+            if (nums[i] != i + 1)
+            {
+                return i + 1;
+            }
+        }
+
+        return nums.size() + 1;
+    }
+};
